4ca9e2313f42953d33664500cdbd744628317b62
Yuri <yuri.manahan@gmail.com>
2017-02-21 21:28:01 -0500

    Works with html and JSON

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+  $('#new_tweet').on('submit', function(event){
+    event.preventDefault();
+    console.log('prevented default');
+
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: $(this).serialize(),
+      dataType: 'json'
+    }).done(function(responseData){
+      console.log("Success");
+      console.log(responseData);
+      $('.tweets').prepend(responseData);
+    }).fail(function(){
+      console.log('failed');
+    }).always(function(){
+      console.log('this always comes back');
+    });
+
+  });
+});
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,10 +7,28 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
 
-    if @tweet.save
-      redirect_to tweets_path
+    if request.xhr?
+      @tweet.save
+
+      respond_to do |format|
+
+        format.json do
+          render json: @tweet
+        end
+
+        format.html do
+          render html: "<li class='tweet'>#{@tweet.message}</br>
+          <time>#{@tweet.created_at.strftime('%b %e, %l:%M %p')}</time></li>".html_safe
+        end
+
+      end
     else
-      render :index
+
+      if @tweet.save
+        redirect_to tweets_path
+      else
+        render :index
+      end
     end
   end
 
